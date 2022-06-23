0cf8e48982cd8393231aef2e84c6c45d94389e08
bottle-honey <honey_sheep@naver.com>
2022-06-24 02:38:24 +0900

    시간표 DB생성

--- timetable/models.py
+++ timetable/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
+class Semester(models.Model):
+    name = models.CharField(max_length=100)
+    
+class Table(models.Model):
+    name = models.CharField(max_length=200)
 
-# Create your models here.
+class Class(models.Model):
+    name = models.CharField(max_length=200)
+    professor = models.CharField(max_length=20)
+    day = models.CharField(max_length=5)
+    start_time = models.IntegerField()
+    end_time = models.IntegerField()
+    classroom = models.CharField(max_length=20)
+    class_id = models.IntegerField(primary_key=True)
