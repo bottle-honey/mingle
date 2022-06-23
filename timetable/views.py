c40147a8d16885c82cddd017dde7f0930e30c105
bottle-honey <honey_sheep@naver.com>
2022-06-24 03:59:14 +0900

    과목 필터링 구현-ing

--- timetable/views.py
+++ timetable/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from .forms import TableForm, ClassForm
 from .models import Table, Class
-# Create your views here.
+
+# 시간표 페이지 
 def timetable(request):
     return render(request, 'timetable.html')
 
+# 시간표 추가
 def timetablecreate(request):
     if request.method=='POST':
         form = TableForm(request.POST)
@@ -15,6 +17,7 @@ def timetablecreate(request):
         form = TableForm()
     return render(request, 'TablePostForm.html',{'form':form})
 
+# 과목 추가
 def classcreate(request):
     if request.method=='POST':
         form = ClassForm(request.POST)
@@ -25,7 +28,12 @@ def classcreate(request):
         form = ClassForm()
     return render(request, 'ClassPostForm.html',{'form':form})
 
+# 과목 리스트 추가
 def classsearch(request):
     classes = Class.objects.filter().order_by('name')
     return render(request, 'ClassList.html', {'classes':classes})
 
+# 과목 필터링
+
+def classfiltering(attribute):
+    pass
